19e600ae7f2c21ba94b0514bce3f13a5b4f66b87
Kamil Szestowicki <Kamil.Szestowicki@Gmail.Com>
2019-03-31 15:03:22 +0200

    main

---
+++ src/main/java/eu/szestkam/springmvcfirst/SpringMvcFirstApplication.java
@@ -0,0 +1,12 @@
+package eu.szestkam.springmvcfirst;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringMvcFirstApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringMvcFirstApplication.class, args);
+	}
+}
