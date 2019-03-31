c6adc46f6eed13c2cb64ad182738a83730cb9358
Kamil Szestowicki <Kamil.Szestowicki@Gmail.Com>
2019-03-31 15:03:44 +0200

    index and product controller

---
+++ src/main/java/eu/szestkam/springmvcfirst/controllers/ProductController.java
@@ -0,0 +1,63 @@
+package eu.szestkam.springmvcfirst.controllers;
+
+import eu.szestkam.springmvcfirst.domain.Product;
+import eu.szestkam.springmvcfirst.services.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+@Controller
+public class ProductController {
+
+    private ProductService productService;
+
+    @Autowired
+    public void setProductService(ProductService productService) {
+        this.productService = productService;
+    }
+
+    @RequestMapping("/products")
+    public String listProducts(Model model){
+        model.addAttribute("products", productService.listAllProducts());
+        return "products";
+    }
+
+    @RequestMapping("/product/{id}")
+    public String getProduct(@PathVariable Integer id, Model model){
+        model.addAttribute("product", productService.getProductById(id));
+
+        return "product";
+    }
+
+    @RequestMapping("/product/edit/{id}")
+    public String editProduct(@PathVariable Integer id, Model model){
+        model.addAttribute("product", productService.getProductById(id));
+
+        return "productForm";
+    }
+
+    @RequestMapping("/product/delete/{id}")
+    public String deleteProduct(@PathVariable Integer id){
+
+        productService.deleteProduct(id);
+
+        return "redirect:/products";
+    }
+
+    @RequestMapping("product/new")
+    public String newProduct(Model model){
+        model.addAttribute("product", new Product());
+        return "productForm";
+    }
+
+    @RequestMapping(value = "/product", method = RequestMethod.POST)
+    public String newOrUpdateProduct(Product product){
+        Product savedProduct = productService.saveOrUpdateProduct(product);
+
+        return "redirect:/product/" + savedProduct.getId();
+    }
+
+}
